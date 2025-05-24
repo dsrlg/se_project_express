01cb4b986e0d3f218a2cafddc4c2b716e3c56192
dsrlg <sri.duvvuri10@gmail.com>
2025-05-23 19:26:56 -0700

    the likes are not part of 12

--- models/clothingItems.js
+++ models/clothingItems.js
@@ -20,20 +20,6 @@ const clothingItems = new mongoose.Schema({
     message: "Link is not valid",
   }
   },
-  owner: {
- type: mongoose.Schema.Types.ObjectId,
- ref: 'user',
- required: true
-},
-likes: [{
- type: mongoose.Schema.Types.ObjectId,
- ref: 'user',
- default: []
-}],
-createdAt: {
- type: Date,
- default: Date.now
-}
 });
 
 module.exports = mongoose.model('clothingItems', clothingItems);
--- routes/clothingItems.js
+++ routes/clothingItems.js
@@ -4,8 +4,8 @@ const {createItem,getItems,updateItem,deleteItem}=require('../controllers/clothi
 
 router.post('/', createItem)
 router.get('/', getItems)
-router.put('/:itemId/likes', updateItem)
-router.delete('/:itemId/likes', deleteItem)
+router.put('/:itemId', updateItem)
+router.delete('/:itemId', deleteItem)
 
 
 module.exports = router;
